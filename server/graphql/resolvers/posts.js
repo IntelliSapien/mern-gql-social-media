0854a7f32e73760f6058e36cd5bac037623c2faf
Abhinav <abhi.aec89@gmail.com>
2021-08-29 15:34:41 +0530

    Added the JWT Token Verification

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -1,6 +1,7 @@
 const { ApolloError } = require("apollo-server");
 const Post = require("../../models/Post");
 const errorCodes = require("./error");
+const { validateToken } = require("../../utils/jwt");
 module.exports = {
   Query: {
     Posts: async (_, { id }) => {
@@ -22,6 +23,37 @@ module.exports = {
         });
       } catch (err) {
         console.error("🚀 ~ file: posts.js ~ line 20 ~ Posts: ~ err", err);
+        throw new ApolloError(err);
+      }
+    },
+  },
+  Mutation: {
+    createPost: async (_, { body }, context) => {
+      const user = validateToken(context);
+      console.log("🚀 ~ file: posts.js ~ line 33 ~ createPost: ~ user", user);
+      let post;
+      try {
+        post = await new Post({
+          body,
+          createdAt: new Date().toISOString(),
+          user: user.id,
+          username: user.username,
+        }).save();
+        return {
+          ...post._doc,
+          id: post._id,
+        };
+      } catch (err) {
+        console.error("🚀 ~ file: posts.js ~ line 40 ~ createPost: ~ err", err);
+        throw new ApolloError(err);
+      }
+    },
+    deletePost: async (_, { id }) => {
+      try {
+        await Post.findByIdAndDelete(id);
+        return true;
+      } catch (err) {
+        console.error("🚀 ~ file: posts.js ~ line 39 ~ deletePost: ~ err", err);
         throw new ApolloError(err);
       }
     },
--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,5 +1,7 @@
 const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server");
 const { SECRET_KEY } = require("../config");
+const errorCodes = require("./../graphql/resolvers/error");
 module.exports.generateToken = function (user) {
   return jwt.sign(
     {
@@ -10,4 +12,25 @@ module.exports.generateToken = function (user) {
     SECRET_KEY,
     { expiresIn: "1h" }
   );
+};
+
+module.exports.validateToken = (context) => {
+  const authHeader = context.req.headers.authorization || "";
+  console.log("🚀 ~ file: jwt.js ~ line 19 ~ authHeader", authHeader);
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log("🚀 ~ file: jwt.js ~ line 22 ~ token", token);
+    if (token) {
+      try {
+        const user = jwt.verify(token, SECRET_KEY);
+        console.log("🚀 ~ file: jwt.js ~ line 26 ~ user", user);
+        return user;
+      } catch (err) {
+        console.error("🚀 ~ file: jwt.js ~ line 29 ~ err", err)
+        throw new AuthenticationError(errorCodes.INVALID_TOKEN);
+      }
+    }
+    throw new Error(errorCodes.TOKEN_ABSENT);
+  }
+  throw new Error(errorCodes.AUTH_HEADER_ABSENT);
 };
