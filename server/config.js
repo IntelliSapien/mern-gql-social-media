c45be86cc7efef7ba30ef56c33ccb0abbfcd5d4c
Abhinav <abhi.aec89@gmail.com>
2021-09-07 22:40:20 +0530

    Dockerfile created

--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 module.exports = {
-    MONGODB: process.env.MONGODB,
-    SECRET_KEY: process.env.SECRET_KEY,
+    MONGODB:
+        process.env.MONGODB ||
+        'mongodb://admin:mongo@localhost:27017/mern-gql-db?retryWrites=true&w=majority',
+    SECRET_KEY: process.env.SECRET_KEY || 'some secret key',
 };
